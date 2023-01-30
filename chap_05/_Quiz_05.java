fa1a6aeebccdd34fb479a6ce264deca6a70f3a28
PyeonMinjun <alswns9810@gmail.com>
2023-01-30 21:17:29 +0900

    편민준 : 01/30

---
+++ chap_05/_Quiz_05.java
@@ -0,0 +1,24 @@
+package chap_05;
+
+public class _Quiz_05 {
+    public static void main(String[] args) {
+
+        int shoose[] = new int[10];
+        for (int i = 0; i < 10; i++) {
+            shoose[i] = 250 + (5 * i);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("사이즈 "+shoose[i]+" (재고 있음)");
+        }
+
+        //for each 사용
+
+        for (int size1 :
+                shoose) {
+            System.out.println("사이즈 " + size1 + " (재고 있음)");
+        }
+
+
+    }
+}
